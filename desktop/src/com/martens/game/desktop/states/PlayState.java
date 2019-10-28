255a946b610ca5ee24db9a555b1e142a8646d052
I400156 <d.schoenmakers@student.fontys.nl>
2019-10-28 12:43:23 +0100

    collision works, player healthbar done

--- desktop/src/com/martens/game/desktop/states/PlayState.java
+++ desktop/src/com/martens/game/desktop/states/PlayState.java
@@ -1,22 +1,23 @@
 package com.martens.game.desktop.states;
 
 import Bullet.Bullet;
+import Bullet.CollisionRect;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 import com.martens.game.desktop.AlienDemo;
-import com.martens.game.desktop.Objects.Bird;
 import com.martens.game.desktop.Objects.Player;
 
 import java.util.ArrayList;
 
 public class PlayState  extends State{
 
-
-    private Bird bird;
     private Player player1;
+
     private Player player2;
+
     private Texture bg;
 
     private int playerSize = 60;
@@ -26,14 +27,11 @@ public class PlayState  extends State{
 
     protected PlayState(GameStateManager gsm) {
         super(gsm);
-        bird = new Bird(50 ,200);
         player1 = new Player("Bird.png", AlienDemo.WIDTH / 20, AlienDemo.HEIGHT / 20);
-        player2 = new Player("Ornstein.jpg", AlienDemo.WIDTH - (AlienDemo.WIDTH / 20), AlienDemo.HEIGHT / 20);
+        player2 = new Player("Ornstein.jpg", (AlienDemo.WIDTH - (AlienDemo.WIDTH / 6)), (AlienDemo.HEIGHT / 7));
         cam.setToOrtho(false, AlienDemo.WIDTH /2, AlienDemo.HEIGHT /2);
         bg = new Texture("melkweg.jpg");
         bullets = new ArrayList<>();
-
-
     }
 
     @Override
@@ -41,23 +39,40 @@ public class PlayState  extends State{
         if(Gdx.input.justTouched())
         {
             if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE) || Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)) {
-                bullets.add(new Bullet(player1.getXPosition(), player1.getYPosition(), Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY()));
+                bullets.add(new Bullet(player1.getPosition().x, player1.getPosition().y, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY()));
             }
         }
         if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE))
         {
             Gdx.app.exit();
         }
-
     }
 
     @Override
     public void update(float dt) {
         handleInput();
 
-        cam.position.x = bird.getPosition().x + 80;
-
         cam.update();
+
+        for (Bullet bullet: bullets) {
+/*            else if(player1.getCollisionRect().collidesWith(bullet.getCollisionRect()))
+            {
+                player1.updateHP((int)bullet.GetVelocity().x / 5);
+            }*/
+            if(player2.getCollisionRect().collidesWith(bullet.getCollisionRect()) && !bullet.hit)
+            {
+                player2.updateHP((int)bullet.GetVelocity().x / 50);
+                bullet.hit = true;
+                Vector3 vector3 = bullet.GetVelocity();
+                Vector3 bounced = new Vector3();
+                bounced.x = 0 - (vector3.x / 2);
+                bounced.y = Math.round(vector3.y / 2) - 50;
+                bounced.z = vector3.z;
+                bullet.updateVelocity(bounced);
+            }
+        }
+        player1.update(dt);
+        player2.update(dt);
 
         removeBullets(dt);
     }
@@ -72,7 +87,6 @@ public class PlayState  extends State{
                 bulletToRemove.add(bullet);
             }
         }
-
         bullets.removeAll(bulletToRemove);
     }
 
@@ -82,8 +96,8 @@ public class PlayState  extends State{
         sb.begin();
 
         sb.draw(bg, 0, 0, AlienDemo.WIDTH, AlienDemo.HEIGHT);
-        sb.draw(player1.getTexture(), player1.getXPosition(), player2.getYPosition(), playerSize, playerSize);
-        sb.draw(player2.getTexture(), player2.getXPosition(), player2.getYPosition(), playerSize, playerSize);
+        player1.render(sb);
+        player2.render(sb);
 
         for (Bullet bullet: bullets) {
             bullet.render(sb);
