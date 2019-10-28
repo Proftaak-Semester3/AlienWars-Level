01330b1c836f7b4fdb0480f8074ac32af42e0e09
wesley <417126@student.fontys.nl>
2019-10-28 14:46:43 +0100

    unit testen

---
+++ core/src/test/java/Bullet/Test.java
@@ -0,0 +1,13 @@
+package Bullet;
+
+import static org.junit.Assert.assertEquals;
+
+public class Test {
+
+    @org.junit.jupiter.api.Test
+
+
+    public void test1() {
+        assertEquals(1,1);
+    }
+}
