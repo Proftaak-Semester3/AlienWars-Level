9e511f6624eaef3777d3fc0db0ce466873892914
Wesley <wesley.verhagen@student.fontys.nl>
2019-10-28 15:05:10 +0100

    Removed duplicate code
    Removed some duplicate code created during branch merging

--- core/src/Bullet/Bullet.java
+++ core/src/Bullet/Bullet.java
@@ -12,10 +12,6 @@ public class Bullet {
 
     private Vector3 position;
 
-    public Vector3 getPosition() {
-        return position;
-    }
-
     private Vector3 velocity;
 
     public Vector3 getPosition() {
