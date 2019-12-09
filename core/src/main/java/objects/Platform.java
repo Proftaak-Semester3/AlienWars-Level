8d970abdb19e649c6a1e764995540956eb5d9d60
Wesley <wesley.verhagen@student.fontys.nl>
2019-12-09 14:36:28 +0100

    Refactored Platform logic
    Refactored logic related to platforms into its own class

---
+++ core/src/main/java/objects/Platform.java
@@ -0,0 +1,50 @@
+package objects;
+
+import bullet.CollisionRect;
+import com.badlogic.gdx.graphics.Texture;
+import org.w3c.dom.Text;
+
+public class Platform {
+
+    CollisionRect collider;
+    Texture texture;
+    float xPosition;
+    float yPosition;
+    int width;
+    int height;
+
+    public CollisionRect getCollider() {
+        return collider;
+    }
+
+    public Texture getTexture(){
+        return texture;
+    }
+
+    public float getXPosition(){
+        return xPosition;
+    }
+
+    public float getYPosition(){
+        return yPosition;
+    }
+
+    public int getWidth(){
+        return width;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+
+    public Platform(float xPos, float yPos, int width, int height, String file){
+        collider = new CollisionRect(xPos, yPos, width, height);
+        texture = new Texture(file);
+
+        xPosition = xPos;
+        yPosition = yPos;
+        this.width = width;
+        this.height = height;
+    }
+
+}
