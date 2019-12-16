294b7793b1b65ab5a93bb172455d126b9b8b9a60
I400156 <d.schoenmakers@student.fontys.nl>
2019-12-16 09:04:45 +0100

    changed ip for presentation

--- core/src/main/java/websockets/WebsocketClient.java
+++ core/src/main/java/websockets/WebsocketClient.java
@@ -8,7 +8,7 @@ import javax.websocket.WebSocketContainer;
 import java.net.URI;
 
 public class WebsocketClient {
-    private final static String uri = "ws://145.93.160.169:8096/alien/";
+    private final static String uri = "ws://145.93.160.187:8096/alien/";
     private Session session;
     private messageCreator messageCreator;
 
