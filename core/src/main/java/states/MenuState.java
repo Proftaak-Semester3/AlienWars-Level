38a7ac9c55979dcedbaa00be53124e1ec9691454
Wesley <wesley.verhagen@student.fontys.nl>
2020-01-13 13:37:11 +0100

    Moved Playbutton

--- core/src/main/java/states/MenuState.java
+++ core/src/main/java/states/MenuState.java
@@ -48,7 +48,7 @@ public class MenuState extends State{
         cam.update();
         sb.setProjectionMatrix(cam.combined);
         sb.draw(background, 0 , 0, AlienDemo.WIDTH, AlienDemo.HEIGHT);
-        sb.draw(playBtn, ((AlienDemo.WIDTH / 2) - (playBtn.getWidth() / 2)), (AlienDemo.HEIGHT / 2));
+        sb.draw(playBtn, ((AlienDemo.WIDTH / 2) - (playBtn.getWidth() / 2)), (AlienDemo.HEIGHT / 2) + 100);
         sb.end();
     }
 
