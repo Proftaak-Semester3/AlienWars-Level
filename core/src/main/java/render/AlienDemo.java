91aa9600b320a450a61deeb5786a6ad92d268e0e
Wesley <wesley.verhagen@student.fontys.nl>
2020-01-20 11:26:58 +0100

    Fixed Code Smells in AlienDemo

--- core/src/main/java/render/AlienDemo.java
+++ core/src/main/java/render/AlienDemo.java
@@ -5,9 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import states.GameStateManager;
-import states.LoginState;
 import states.MenuState;
-import states.RegisterState;
 
 public class AlienDemo extends ApplicationAdapter {
     public static final int WIDTH = 1920;
